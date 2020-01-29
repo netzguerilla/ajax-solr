a38a03c9522156b7a8dc05288c748525206976c0
Sandro Knauß <sknauss@kde.org>
2020-01-29 10:29:07 +0100

    clenaup

--- examples/fce/js/fce.js
+++ examples/fce/js/fce.js
@@ -80,8 +80,8 @@ define([
       'qf': 'title_txt_de^3 body_txt_de^2 author_txt_de text_fr',
       'stopwords': true,
       'facet.range': 'pub_date',
-      'facet.range.start': '2009-02-26T00:00:00.000Z/DAY',
-      'facet.range.end': '2019-10-20T00:00:00.000Z/DAY',
+      'facet.range.start': '2009-02-1T00:00:00.000Z/DAY',
+      'facet.range.end': '2019-10-31T00:00:00.000Z/DAY',
       'facet.range.gap': '+1MONTH'
     };
     for (var name in params) {
